3b1d90194ad8a59e1a56b6a1d2d13f251d3f2ef6
Francesco-Pineschi <francesco.pineschi@archinnova.org>
2025-05-14 17:04:49 +0200

    prima release pubblica

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  title = 'ReAngular';
+  description = 'ReAngular is a modular Single Page Application (SPA) built with Angular and Bootstrap. It uses client-side routing and a scalable architecture based on feature modules.';
 }
