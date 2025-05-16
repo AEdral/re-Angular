6a235124ae0389faca6ee01806511b52b3e3fa51
Francesco-Pineschi <francesco.pineschi@archinnova.org>
2025-05-16 12:59:20 +0200

    aggiunto un sotto modulo funzionante visibile dal routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { TestComponent } from './core/test/test.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'test', component: TestComponent },
   { path: '**', redirectTo: '' },  // Redirect to home if no route is matched
 ];
 
